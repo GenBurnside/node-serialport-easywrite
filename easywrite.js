c5e751bd1b178fa79549c73d72e17c9cdebf926a
Matt Hughes <matthughes.tech@gmail.com>
2015-03-19 16:57:12 -0400

    Fix whitespace weirdness

--- easywrite.js
+++ easywrite.js
@@ -1,22 +1,22 @@
 var serialPort = require("serialport"),
-		SerialPort = require("serialport").SerialPort,
-		config = require("./config.js");
+    SerialPort = require("serialport").SerialPort,
+    config = require("./config.js");
 
 var sp = new SerialPort(config.serialPort, {
-	baudrate: config.baudRate
+  baudrate: config.baudRate
 }, false);
 
 function write(message) {
-	sp.open(function(err) {
-		sp.write(message, function(err, res) {
-			if (err) {
-				console.log(err);
-			}
-			sp.close();
-		});
-	});
+  sp.open(function(err) {
+    sp.write(message, function(err, res) {
+      if (err) {
+        console.log(err);
+      }
+      sp.close();
+    });
+  });
 }
 
 module.exports = {
-	write: write
+  write: write
 };
